923e10cc36759987a2fd882faad5ea72681b9df1
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-04-25 08:39:55 +0200

    Change from debug to info logging.

--- gateway/exporters/kafka/kafka.go
+++ gateway/exporters/kafka/kafka.go
@@ -56,7 +56,7 @@ func (e *KafkaExporter) Name() string {
 }
 
 func (e *KafkaExporter) Export(leaf *ctree.Leaf) {
-	e.config.Log.Debug().Msg("In kafka Export function now.")
+	e.config.Log.Info().Msg("In kafka Export function now.")
 	notification := leaf.Value().(*gnmipb.Notification)
 
 	data, err := proto.Marshal(notification)
